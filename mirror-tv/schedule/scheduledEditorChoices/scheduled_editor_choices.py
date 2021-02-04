f49480d85cae0384f0bb00ec3be236dff815d842
Baron Chiu <baronchiu@mirrormedia.mg>
2021-02-04 21:36:25 +0800

    wip(scheduledEditorChoices): implement query and the state modification algorithm

--- mirror-tv/schedule/scheduledEditorChoices/scheduled_editor_choices.py
+++ mirror-tv/schedule/scheduledEditorChoices/scheduled_editor_choices.py
@@ -19,6 +19,17 @@ __gql_client__ = Client(
     fetch_schema_from_transport=True,
 )
 
+# TODO email and password must be provided from external source
+# Authenticate through GraphQL
+__qgl_mutation_authenticate_get_token__ = '''
+mutation {
+  authenticate: authenticateUserWithPassword(email: "%s", password: "%s") {
+    token
+  }
+}
+'''
+
+
 # To query the EditorChoices in state of published and scheduled, so it can tolling update their state
 __qgl_query_editor_choices_to_modify__ = '''
 {
@@ -29,15 +40,23 @@ __qgl_query_editor_choices_to_modify__ = '''
 }
 '''
 
-# TODO email and password must be provided from external source
-# Authenticate through GraphQL
-__qgl_mutation_authenticate_get_token__ = '''
-mutation {
-  authenticate: authenticateUserWithPassword(email: "%s", password: "%s") {
-    token
-  }
-}
-'''
+query = gql(__qgl_query_editor_choices_to_modify__)
+editor_choices = __gql_client__.execute(query)['allEditorChoices']
+
+
+def get_updated_state_value(state):
+    states_waterfall = ['scheduled', 'published', 'draft']
+
+    # update the state value according to the original state, only scheduled and published will not cause an exception
+    # if state is not scheduled and published, then it exception happens and we do not update it
+    try:
+        return states_waterfall[states_waterfall.index(state) + 1]
+    except:
+        return state
+
+
+new_data_str = str([{'id': editor_choice['id'], 'state': get_updated_state_value(editor_choice['state'])}
+                    for editor_choice in editor_choices])
 
 # To update EditorChoices, data should be an array of objects containing id, and data
 __qgl_mutate_editor_choices_template__ = '''
