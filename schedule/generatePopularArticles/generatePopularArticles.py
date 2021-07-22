0644daee6e9fb7d4efc1f5a4f51fe36c2fd6ae08
Baron Chiu <21715194+bcgodev@users.noreply.github.com>
2021-07-22 15:25:25 +0800

    fix(generatePopularArticles): handle undefined 'rows' in reports data (#103)
    * fix(generatePopularArticles): handle undefined 'rows' in reports data

--- schedule/generatePopularArticles/generatePopularArticles.py
+++ schedule/generatePopularArticles/generatePopularArticles.py
@@ -168,9 +168,12 @@ def upload_blob(bucket_name: str, destination_blob_name: str, report: bytes):
 def convert_response_to_report(config_graphql: dict, slugBlacklist: list, date_range: tuple, response: dict) -> str:
     '''Parse the response and generate the json format file for it'''
     result = {}
-    data = response['reports'][0]['data']['rows']
-    data = sorted(
-        data, key=lambda x: int(x['metrics'][0]['values'][0]), reverse=True)
+    data = []
+    if 'rows' in response['reports'][0]['data']:
+        data.extend(response['reports'][0]['data']['rows'])
+        data = sorted(
+            data, key=lambda x: int(x['metrics'][0]['values'][0]), reverse=True)
+
     slugs = [item['dimensions'][0].replace(
         '/', '') for item in data if item['dimensions'][0].replace('/', '') not in slugBlacklist]
 
